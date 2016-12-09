81bbb80d049820b1322ac3697d3726d19ebdbab4
Bret Johnson <bret.johnson@microsoft.com>
2016-12-09 13:08:53 -0800

    Initial checkin

---
+++ iOSNativeWithTests/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSNativeWithTests
+//
+//  Created by Bret Johnson on 12/9/16.
+//  Copyright © 2016 Bret Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSNativeWithTests/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSNativeWithTests
+//
+//  Created by Bret Johnson on 12/9/16.
+//  Copyright © 2016 Bret Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
